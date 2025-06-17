b784e657615e19f483f2469fbf354612b5e1eef8
youssef-rafie212 <youssefelbosaty3@gmail.com>
2025-06-17 17:33:09 +0300

    cleaning apiError

--- src/helpers/apiError.js
+++ src/helpers/apiError.js
@@ -1,84 +1,44 @@
-class ApiError extends Error{
-    constructor(key,code,message){
-        super();
-        this.key     = key;
-        this.code    = "" ;
-        this.message = message ;
-    }
-
-    static NotFound(message,key,code) {
-        this.key     = key;
-        this.code    = this.GetCode(key);
-        this.message = message;
-    }
-
-    static BadRequest(message,key,code) {
-        this.key     = key ;
-        this.code    = this.GetCode(key);
-        this.message = message;
-    }
-
-    static UnprocessableEntity(message,key,code) {
-        this.key     = key ;
-        this.status  = code;
-        this.message = message;
-    }
-
-    static Forbidden(message,key,code) {
-        this.key     = key ;
-        this.code    = this.GetCode(key);
-        this.message = message;
-    }
-    static Exception(message,key,code) {
-        this.key     = key ;
-        this.code    = this.GetCode(key);
-        this.message = message;
-    }
-
-
-
-    GetCode(key)
-    {
-        let code =""
+class ApiError {
+    GetCode(key) {
+        let code = "";
         switch (key) {
-            case 'success':
+            case "success":
                 code = 200;
                 break;
-            case 'fail':
+            case "fail":
                 code = 400;
                 break;
-            case 'needActive':
+            case "needActive":
                 code = 203;
                 break;
-            case 'unauthorized':
+            case "unauthorized":
                 code = 401;
                 break;
-            case 'unauthenticated':
+            case "unauthenticated":
                 code = 401;
                 break;
-            case 'blocked':
+            case "blocked":
                 code = 423;
                 break;
-            case 'notFound':
+            case "notFound":
                 code = 404;
                 break;
-            case 'exception':
+            case "exception":
                 code = 500;
                 break;
-            case 'forbidden':
+            case "forbidden":
                 code = 403;
                 break;
 
             default:
                 code = 200;
                 break;
-
         }
 
         return code;
     }
-    responseError(key,message){
-        return {key : key, message: message, code : this.GetCode(key)}
+    responseError(key, message) {
+        return { key: key, message: message, code: this.GetCode(key) };
     }
 }
 
